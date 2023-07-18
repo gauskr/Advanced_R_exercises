b101e1d9a32fee42ec7a32604fb392da4f7840b2
gauskr <gaute_skrove@hotmail.com>
2023-07-18 20:59:03 +0200

    Practicing ch 6 functions

--- ADV_R_6_Functions.R
+++ ADV_R_6_Functions.R
@@ -7,4 +7,32 @@
 
 # 6.2.1 Function components -----------------------------------------------
 
+f02 <- function(x, y) {
+  # A comment
+  x + y
+}
 
+formals(f02)
+body(f02)
+environment(f02)
+attr(f02, "srcref")
+
+# 6.2.2 Primitive functions -----------------------------------------------
+
+sum # Primitive functions calls C code directly.
+`[`
+typeof(sum)
+typeof(`[`)
+formals(sum)
+body(sum)
+environment(sum)
+
+# 6.2.3 First-class functions ---------------------------------------------
+
+f01 <- function(x) {
+  sin(1 / x ^ 2)
+}
+
+lapply(mtcars, function(x) length(unique(x)))
+Filter(function(x) !is.numeric(x), mtcars)
+integrate(function(x) sin(x) ^ 2, 0, 1)
